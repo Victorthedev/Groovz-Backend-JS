9b6242d078d3161f7ff0760758daeb6d01ffd66b
Victorthedev <ubahakweemeka@gmail.com>
2024-07-21 00:50:30 +0100

    improvements

--- utils/spotifyApi.js
+++ utils/spotifyApi.js
@@ -1,9 +1,9 @@
 const SpotifyWebApi = require('spotify-web-api-node');
 
 const spotifyApi = new SpotifyWebApi({
-    clientId:'5e3eef3570b74a37af3438268b820e32',
-    clientSecret:'ecda63e51490449d9c94b26f9fd9571a',
-    redirectUri:'http://localhost:4000/auth/callback',
+    clientId: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    redirectUri: process.env.SPOTIFY_REDIRECT_URI,
 });
 
 module.exports = spotifyApi;
