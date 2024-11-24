dc92f1610c85a18a3bd3e565c118bcdf50c07166
Victor <ubahakweemeka@gmail.com>
2024-11-24 16:12:59 +0100

    live url

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://main.d1n7z7zw3v28b1.amplifyapp.com'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
