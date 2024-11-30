77ac9cbcba3284942c0458c1ceb787926feaf491
Victor <ubahakweemeka@gmail.com>
2024-11-30 01:24:36 +0100

    commits

--- utils/spotifyApi.js
+++ utils/spotifyApi.js
@@ -3,7 +3,7 @@ const SpotifyWebApi = require('spotify-web-api-node');
 const spotifyApi = new SpotifyWebApi({
     clientId: '5e3eef3570b74a37af3438268b820e32',
     clientSecret: 'ecda63e51490449d9c94b26f9fd9571a',
-    redirectUri: 'http://localhost:4000/auth/callback',
+    redirectUri: 'https://groovz-backend-js.onrender.com/auth/callback',
 });
 
 module.exports = spotifyApi;
